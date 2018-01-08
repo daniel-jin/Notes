323fa85ee46121ef5cc0ea5a43bf83a44765f23d
daniel-jin <daniel.h.jin@gmail.com>
2018-01-08 17:40:20 -0600

    Storyboard elements & segues are set up

---
+++ Notes/Notes/Note.swift
@@ -0,0 +1,15 @@
+//
+//  Note.swift
+//  Notes
+//
+//  Created by Daniel Jin on 1/8/18.
+//  Copyright © 2018 Daniel Jin. All rights reserved.
+//
+
+import Foundation
+
+class Note {
+    
+    
+    
+}
